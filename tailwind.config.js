0f302decd868d9686d2e2ddfd56344f0881becbc
Michelle <michellarass@gmail.com>
2022-02-18 02:05:48 +0700

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,9 @@ module.exports = {
   content: [ './public/index.html',
               './src/**/*.{html,js}',
             ],
-            theme: {
-              screens: {
-                sm: '480px',
-                md: '768px',
-                lg: '976px',
-                xl: '1440px',
+              theme: {
+                extend: {},
               },
-              colors: {
-                'blue': '#1fb6ff',
-                'pink': '#ff49db',
-                'orange': '#ff7849',
-                'green': '#13ce66',
-                'gray-dark': '#273444',
-                'gray': '#8492a6',
-                'gray-light': '#d3dce6',
-              },
-              extend: {
-                spacing: {
-                  '128': '32rem',
-                  '144': '36rem',
-                },
-                borderRadius: {
-                  '4xl': '2rem',
-                }
-              }
-            },
-  plugins: [],
-}
+              plugins: [],
+            }
+     
