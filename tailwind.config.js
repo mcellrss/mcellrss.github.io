a49c7ec20cceadf6b1b6fe895a32e7318bbe2cd3
Michelle <michellarass@gmail.com>
2022-12-09 23:55:46 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+    
+    },
+  },
+  // ...
+}
+
 module.exports = {
   content: [ './public/index.html',
               './src/**/*.{html,js}',
             ],
               theme: {
-                extend: {
       extend: {
         gridTemplateRows: {
           '[auto,auto,1fr]': 'auto auto 1fr',
         },
+  fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       },
     },
               },
